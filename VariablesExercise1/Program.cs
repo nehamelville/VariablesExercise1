0ac1aa6cec8754d8c2fa475e30db687df8643bfd
nehamelville <neha.melville@gmail.com>
2021-09-27 17:45:56 -0700

    source code modified

--- VariablesExercise1/Program.cs
+++ VariablesExercise1/Program.cs
@@ -7,22 +7,24 @@ namespace VariablesExercise1
         static void Main(string[] args)
         {
 
-            string dogName;
-            int dogAge;
-            char s;
-            bool isAlive = true;
-            double dogLength = 25;
-            decimal dogWeight = 66;
+            string dogName = "Raulph";
+            int dogAge = 4;
+            char initial = 'N';
+            bool isDalmation = true;
+            double dogLength = 25.4;
+            decimal dogWeight = 66.4m;
           
-            Console.Write("What is your dog name?");
+            /*
+              Console.Write("What is your dog name? ");
             dogName = Console.ReadLine();
-
-            Console.Write($"What is {dogName} age?");
+            Console.Write($"What is {dogName} age? ");
             dogAge = Convert.ToInt32(Console.ReadLine());
+            */
 
-
-            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
-            Console.WriteLine($"{dogName} weighs {dogWeight}, And dog is {dogLength} inches");
+            Console.WriteLine($"My dog's name is {dogName} {initial}. " +
+                $"He is {dogAge} years old. {dogName} weighs {dogWeight}lbs " +
+                $"and {dogLength} inches long and he is {(isDalmation ? "a Dalmation" : "unknown")}.");
+            
            
         }
     }
